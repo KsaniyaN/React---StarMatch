d7cccb3161cc8a4cacb7cd717570e092029dcfab
KsaniyaN <ksaniya@gmail.com>
2019-07-30 16:00:22 +1000

    Updates

---
+++ precache-manifest.229f1b328ed3c34963cc5106ae36f3c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61101092f6dc3c35fd13fcf6f7b78075",
+    "url": "/React---StarMatch/index.html"
+  },
+  {
+    "revision": "2bf7a2553bba5fcdafb1",
+    "url": "/React---StarMatch/static/css/main.49698a61.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/React---StarMatch/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "2bf7a2553bba5fcdafb1",
+    "url": "/React---StarMatch/static/js/main.824f20bd.chunk.js"
+  },
+  {
+    "revision": "bef846a72e06b10d1fa6",
+    "url": "/React---StarMatch/static/js/runtime~main.507aa5aa.js"
+  }
+]);
